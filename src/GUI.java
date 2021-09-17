7e392d03c64d4e552c749412a194cfc19b63197e
ItsEricSun <78322291+ItsEricSun@users.noreply.github.com>
2021-09-16 20:48:27 -0400

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -115,6 +115,7 @@ public class GUI extends GBFrame {
 	    	mainMenu();
 	    	return;
 	    } else if (buttonObj == removeStudentButton) {
+		it = Class.iterator();
 	    	String name = nameField.getText();
 	    	if(name.equals("")) {
 	    		messageBox("Error: Please Enter Name");
